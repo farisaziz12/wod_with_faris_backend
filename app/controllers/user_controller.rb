4ac27aa2c075f0126f289188ea6e717e53173ee0
Faris Aziz <farisaziz12@gmail.com>
2020-03-28 00:08:34 +0100

    user controller, table and model created

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  get 'user/create'
+  get 'user/new'
+  get 'user/edit'
+  get 'user/update'
+  get 'user/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
