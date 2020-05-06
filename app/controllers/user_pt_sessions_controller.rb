4e108ffe0eb33f5342d8ab056d004b12ab529f27
Faris Aziz <farisaziz12@gmail.com>
2020-05-06 23:07:20 +0200

    user_pt_session get route changed

--- app/controllers/user_pt_sessions_controller.rb
+++ app/controllers/user_pt_sessions_controller.rb
@@ -2,7 +2,7 @@ class UserPtSessionsController < ApplicationController
 
 
     def get_user_pt_sessions
-        user = User.find_by(email: params[:email])
+        user = User.find(params[:id])
         if user.coach == true
             allptsessions = Ptsession.where(user_id: user.id)
             pt_sessions_this_week = allptsessions.select { |ptsession| ptsession.date.between?(Date.current, Date.current + 7) }
