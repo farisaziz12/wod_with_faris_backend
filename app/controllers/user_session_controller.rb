25eef205b3cb6b2e6834b781a8a23ddd53e6d857
farisaziz12 <farisaziz12@gmail.com>
2020-05-30 00:01:24 +0200

    no longer need to account for currently booked class

--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -28,7 +28,7 @@ class UserSessionController < ApplicationController
     def user_prev_sessions_current_tokens
         user = User.find(params[:id])
         sessions_prev_week = user.sessions.select { |session| session.date.between?(Date.current - 7, Date.current + 7)}
-        render json: { sessions_prev_week: sessions_prev_week.length + 1, tokens: user.tokens }
+        render json: { sessions_prev_week: sessions_prev_week.length, tokens: user.tokens }
     end
 
 
